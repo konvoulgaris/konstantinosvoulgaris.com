3b05c9ef9f553bf89faaf70a5f2242e9441d6cbf
Konstantinos Voulgaris <36533602+konvoulgaris@users.noreply.github.com>
2022-08-26 15:19:45 +0300

    Add theme switcher

--- assets/js/index.js
+++ assets/js/index.js
@@ -8,3 +8,41 @@ const getCopyrightYearSpan = (start) => {
         copyright.innerHTML = `${start} - ${current}`;
     }
 }
+
+const toggleTheme = () => {
+    let theme = localStorage.getItem("theme");
+    
+    if (theme == null) {
+        const isDark = document.body.classList.contains("dark");
+        theme = isDark ? "light" : "dark";
+    } else {
+        theme = theme === "light" ? "dark" : "light";
+    }
+
+    localStorage.setItem("theme", theme);
+    setTheme(theme);
+}
+
+const getTheme = () => {
+    const theme = localStorage.getItem("theme");
+
+    if (theme == null) {
+        const preference = window.matchMedia("(prefers-color-scheme: dark)").matches;
+        setTheme(preference ? "dark" : "light");
+    } else {
+        setTheme(theme);
+    }
+}
+
+const setTheme = (theme) => {
+    if (theme === "light") {
+        document.body.classList.toggle("dark", false);
+    } else {
+        document.body.classList.toggle("dark", true);
+    }
+}
+
+window.matchMedia("(prefers-color-scheme: dark)").addEventListener("change", (e) => {
+    const theme = e.matches ? "dark" : "light";
+    setTheme(theme);
+});
