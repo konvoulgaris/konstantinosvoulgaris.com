b889fe6e7b1f46728f8a79ab8c03f7007ee7df56
Konstantinos Voulgaris <36533602+konvoulgaris@users.noreply.github.com>
2022-08-28 17:07:20 +0300

    Add greeting logic

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,3 +1,16 @@
+const getGreeting = () => {
+    const time = new Date().getHours();
+    const greeting = document.getElementById("greeting");
+
+    if (time >= 5 && time < 12) {
+        greeting.innerText = "Good Morning!";
+    } else if (time >= 12 && time < 18) {
+        greeting.innerText = "Good Afternoon!";
+    } else {
+        greeting.innerText = "Good Evening!";
+    }
+}
+
 const getCopyrightYear = (start) => {
     const current = new Date().getFullYear();
     const copyright = document.getElementById("copyright");
